0dd367ef5f5073c2935dffcee0a7e792c9738071
Gustavo <kyoro_souza@hotmail.com>
2020-03-26 21:58:03 -0300

    update 1.3

--- frontend/heroweb/src/pages/Register/index.js
+++ frontend/heroweb/src/pages/Register/index.js
@@ -14,7 +14,7 @@ export default function Register(){
                     <img src={logoImg} alt="Be The Hero"></img>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas aa encontrarem os casos da sua ONG.</p>
-                    <Link className='back-link' to="/register">
+                    <Link className='back-link' to="/">
                         <FiArrowLeft size={16} color="#E02041"/>
                         Não tenho cadastro
                     </Link>
@@ -27,7 +27,7 @@ export default function Register(){
                         <input placeholder="Cidade"/>
                         <input placeholder="UF" style={ { width: 80 } }/>
                     </div>
-                    <button></button>
+                    <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
