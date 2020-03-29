eae76da2017995be560a7576a21e993fce214c5f
Gustavo <kyoro_souza@hotmail.com>
2020-03-29 13:19:02 -0300

    ultima atualização

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 
 const app = express();
@@ -9,5 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,6 @@
 
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId')
+
 const connection = require('../database/connection');
 
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
     async create(req, res) {
         const { name, email, whatsapp, city, uf } = req.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
